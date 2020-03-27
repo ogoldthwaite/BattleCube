c5c332148674edef7013e42b2f0464bb1cc797c5
Owen <owengoldthwaite@gmail.com>
2020-03-27 17:03:27 -0400

    Initial Player Class Thing

---
+++ Assets/Scripts/ParentCharacterClass.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentCharacterClass : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody rb;
+    private CharacterMovement moveScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveScript = new CharacterMovement();
+        rb = GetComponent<Rigidbody>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            moveScript.Jump(rb, jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveScript.MoveForward(rb, moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveScript.MoveBackward(rb, moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveScript.MoveLeft(rb, moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveScript.MoveRight(rb, moveSpeed);
+        }
+
+    }
+
+}
