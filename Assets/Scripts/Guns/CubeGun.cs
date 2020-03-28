22ab8c282dbb5f8271bd031e841b64cd9c525ef9
Owen <owengoldthwaite@gmail.com>
2020-03-28 17:10:07 -0400

    PickUp Stuff

--- Assets/Scripts/Guns/CubeGun.cs
+++ Assets/Scripts/Guns/CubeGun.cs
@@ -7,7 +7,7 @@ public class CubeGun : ParentGunClass
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* Take a look back at this script if/when multiplayer functionality gets added.
+ * It will probably need a pretty significant overhaul.
+ **/
+public class PickupItem : MonoBehaviour
+{
+    public GameObject item;
+    public int itemCount = 1;
+    public Vector3 colliderSize = new Vector3(1, 1, 1);
+
+
+    private bool playerInRange = false;
+    private List<GameObject> playerList = new List<GameObject>(); // List of players that are within range, first one in will get priority
+
+    void Start()
+    {
+        BoxCollider pickupRange = gameObject.AddComponent<BoxCollider>();
+        pickupRange.center = new Vector3(0, 0, 0);
+        pickupRange.size = colliderSize;
+        pickupRange.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If player is in range to grab and at least 1 item left and E is being pressed
+        if (playerInRange && itemCount > 0 && Input.GetKey(KeyCode.E))
+        {
+            itemCount--;
+            GameObject player = playerList[0];
+            Vector3 spawnPos = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z);
+            GameObject itemInstance = Instantiate(item, spawnPos, Quaternion.identity);
+            itemInstance.transform.parent = player.transform;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+            playerList.Add(col.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+            playerList.Remove(col.gameObject);
+        }
+    }
+}
